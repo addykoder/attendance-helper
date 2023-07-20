278933bc09a7d46a7f259c2ab2af60d0bd7d6111
Aditya Tripathi <productive.py@gmail.com>
2023-07-20 23:39:48 +0530

    updated auto updating base url

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/attendance-helper/sw.js', { scope: '/attendance-helper/' })})}
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base:'/attendance-helper',
 	server: {
 		port: 3000,
 		host: true
